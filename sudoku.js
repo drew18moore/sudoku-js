ca5bbc8d54b2acf8c48d28d3dd2df729111e5b46
Drew <dcm752052@gmail.com>
2022-05-19 22:30:48 -0400

    Gave startin tiles a distinct color. Added horizontal and vertical lines to board. Tweaked color of tile borders

--- sudoku.js
+++ sudoku.js
@@ -42,11 +42,19 @@ function setGame() {
         document.getElementById("digits").appendChild(number);
     }
 
-    // Board
+    // Board 9x9
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
             let tile  = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start")
+            }
+            if (r == 2 || r == 5)
+                tile.classList.add("horizontal-line");
+            if (c == 2 || c == 5)
+                tile.classList.add("vertical-line");
             tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("board").appendChild(tile);
